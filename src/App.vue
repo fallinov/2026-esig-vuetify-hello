789c5a28fa20e19606efa74d58e68d5764b591be
Steve Fallet <steve.fallet@divtec.ch>
2026-01-28 11:53:56 +0100

    affichage liste pokémon et v-if

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,63 @@
+<script setup>
+  // import { computed, ref } from 'vue'
+  const newPokemon = ref('Charizard')
+  const pokemons = ref([
+    { name: 'Pikachu', type: 'Electric' },
+    { name: 'Bulbasaur', type: 'Grass' },
+    { name: 'Charmander', type: 'Fire' },
+    { name: 'Squirtle', type: 'Water' },
+    { name: 'Jolteon', type: 'Electric' },
+  ])
+
+  // TODO: ref pour le nom
+  // TODO: ref pour le filtre
+  // TODO: computed filteredPokemons
+  // TODO: function addPokemon()
+  function addPokemon () {
+    alert('Coucou !')
+  }
+</script>
+
 <template>
-  <h1>{{ message }}</h1>
-  <input v-model="message" type="text">
-  <p>Coucou GitHub ;)</p>
+  <main>
+    <h1>Mon Pokédex</h1>
+    <p>{{ newPokemon }}</p>
+    <!-- TODO: input + button -->
+    <div>
+      <input v-model="newPokemon" type="text">
+      <button @click="addPokemon">Ajouter</button>
+    </div>
+    <!-- TODO: checkbox filtre -->
+    <!-- TODO: v-for liste -->
+    <h2>Mes pokémons</h2>
+    <p v-if="pokemons.length === 0">Aucun pokémon attrapé !</p>
+    <ul v-else>
+      <li v-for="p in pokemons" :key="p.name">
+        {{ p.name }}
+        ({{ p.type }})
+      </li>
+    </ul>
+
+  </main>
 </template>
 
-<script setup>
-  const message = ref('Bonjour à tous !')
-</script>
+<style scoped>
+  main {
+    padding: 20px;
+  }
+
+  h1 {
+    color: orange;
+  }
+
+  input {
+    border: 2px solid orange;
+    padding: 5px;
+  }
+
+  button {
+    border: 2px solid orange;
+    padding: 5px;
+    margin:  0 1em;
+  }
+</style>
