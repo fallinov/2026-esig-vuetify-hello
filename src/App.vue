a851abc108e138c7105c183ea873b9caa31c44ed
Steve Fallet <steve.fallet@divtec.ch>
2026-01-28 09:26:19 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>{{ message }}</h1>
+  <input v-model="message" type="text">
+  <p>Coucou 55</p>
+</template>
+
+<script setup>
+  const message = ref('Bonjour à tous !')
+</script>
